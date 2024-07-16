4ca1f5427994708ca0a9adbfbed70872eadaf117
admin <>
2024-07-16 11:32:33 +0000

    {/src/main/java/com/ingramcontent/dataverse/utility/ContextUtility.java}

--- src/main/java/com/ingramcontent/dataverse/utility/ContextUtility.java
+++ src/main/java/com/ingramcontent/dataverse/utility/ContextUtility.java
@@ -139,13 +139,14 @@ public class ContextUtility implements java.io.Serializable {
 			case Constant.CONTAINER_ID:
 				setProcessVariable(context, Constant.CONSUMER_URL, value);
 			break;
-			case Constant.THRESHOLD:
-				setProcessVariable(context, Constant.CONSUMER_URL, value);
-			break;
+
 			case Constant.PIPELINE_CODE_+index:
 				setProcessVariable(context, Constant.CONSUMER_URL, value);
 			break;
 			case Constant.DATAFILTER_+index:
+				setProcessVariable(context, Constant.CONSUMER_URL, value);
+			break;
+			case Constant.THRESHOLD:
 				setProcessVariable(context, Constant.CONSUMER_URL, value);
 			break;
 		}
